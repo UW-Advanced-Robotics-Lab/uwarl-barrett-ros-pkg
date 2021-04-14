4c8752f45f7c08ec1a7a99b4514bee580e06deb3
Brian Zenowich <bz@barrett.com>
2021-04-14 15:25:40 -0400

    Updated launch file for Noetic

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 cd ../..
 #catkin_make clean
 source devel/setup.bash
-catkin_make install --pkg wam_srvs
-catkin_make install --pkg wam_msgs
+
+# Make the deps first
+catkin_make --pkg wam_srvs
+catkin_make --pkg wam_msgs
+
+# Make the rest
+catkin_make
+
+# Install everything
 catkin_make install
